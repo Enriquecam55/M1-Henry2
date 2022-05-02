87f73971b5c2bb0e7640503e4a63035609ac26eb
Enriquecam55 <cat.ehuerta@gmail.com>
2022-05-02 00:24:01 -0500

    homework 1 finished

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -3,10 +3,24 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   // algo
+  let arrayBin = num.split('');
+
+  let sum = 0;
+  for(let i = 0; i < arrayBin.length; i++) {
+    sum = sum + Math.pow(2, arrayBin.length - 1 - i) * arrayBin[i];
+  }
+  return sum; 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+ let arrayBina = [];
+
+ while(num > 0) {
+   arrayBina.unshift(num%2);
+    num = Math.floor(num/2);
+ }
+ return arrayBina.join(''); 
 }
 
 // No se pueden usar: 
